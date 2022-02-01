99e0a3b37f5bd57ee55937257440d869299a7c0c
ame <25921047+zd613@users.noreply.github.com>
2022-02-01 22:01:50 +0900

    prettierの設定作成&追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,8 @@ module.exports = {
     "extends": [
         "eslint:recommended",
         'plugin:vue/vue3-recommended',
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "prettier"
     ],
     "parser": "vue-eslint-parser",
     "parserOptions": {
